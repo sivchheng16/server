dd11b27f8c84b120444fc9de5fcd992dc14985ff
sivchheng16 <kheangsivechheng@gmail.com>
2026-05-04 11:37:43 +0700

    feat: intergrate Oauth sign with google

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,8 @@ const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || "/api/auth/google/callback",
+    proxy: true
   },
   async function(accessToken, refreshToken, profile, cb) {
       try {
